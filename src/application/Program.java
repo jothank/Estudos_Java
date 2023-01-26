cbb7baa058c640e1ff213c9fb22a928e9641ac1f
Jonathan Carlos Costa <jothank@hotmail.com>
2023-01-26 16:14:45 -0300

    up

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
-import java.util.Map;
+import entities.Triangulo;
+
 import java.util.Scanner;
 
 public class Program {
@@ -8,29 +9,33 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        Triangulo x, y;
+        x = new Triangulo();
+        y = new Triangulo();
 
         System.out.println("Entre com a medida do triangulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Entre com a medida do triangulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         System.out.printf("Area do Triangulo X é: %.4f%n", areaX);
         System.out.printf("Area do Triangulo Y é: %.4f%n", areaY);
 
         if (areaX > areaY) {
             System.out.println("Maior area é X");
+        } else if (areaX == areaY) {
+            System.out.println("Areas com os mesmos tamanhos");
         } else {
             System.out.println("Maior area é Y");
         }
